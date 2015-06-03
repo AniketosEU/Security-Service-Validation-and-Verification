451a7894bd345c1fce1e5e20545000e3fea217b8
Achim D. Brucker <adbrucker@0x5f.org>
2015-06-03 08:46:52 +0200

    Initial commit.

---
+++ CSVM-Modelling-and-Validation/src/eu.aniketos.securebpmn.xacml.pdp/src/main/java/eu/aniketos/securebpmn/xacml/pdp/idHandler/IDHandler.java
@@ -0,0 +1,59 @@
+/* Copyright 2012-2015 SAP SE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.aniketos.securebpmn.xacml.pdp.idHandler;
+
+//import java.util.Date;
+//
+//import eu.aniketos.securebpmn.xacml.idm.AuthInfo;
+//import eu.aniketos.securebpmn.xacml.idm.IDInfo;
+
+public class IDHandler {
+
+//	private IDInfoCache cache;
+//
+//	private int cacheEntryMinValid = 60;
+//
+//	public IDHandler() {
+//		cache = new IDInfoCache();
+//	}
+//
+//
+//	public IDInfo resolveID(AuthInfo authinfo) {
+//		// TODO authInfo cache?
+//		IDInfo idInfo = queryIDProvider(authinfo);
+//
+//		IDInfoCacheEntry cacheEntry = cache.getEntry(idInfo);
+//
+//		if ( cacheEntry != null ) {
+//			long now = new Date().getTime();
+//
+//			if ( true ) { // check if entry older than cacheEntryMinValid
+//				//return cacheEntry.foo;
+//			}
+//		}
+//		IDInfo info = queryIDManager(idInfo);
+//		cache.putEntry(info);
+//		return info;
+//	}
+//
+//	private IDInfo queryIDProvider(AuthInfo auhtinfo) {
+//		return null;
+//	}
+//
+//	private IDInfo queryIDManager(IDInfo idInfo) {
+//		return null;
+//	}
+}
---
+++ CSVM-Modelling-and-Validation/src/eu.aniketos.securebpmn.xacml.pdp/src/main/java/eu/aniketos/securebpmn/xacml/pdp/idHandler/IDInfoCache.java
@@ -0,0 +1,69 @@
+/* Copyright 2012-2015 SAP SE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.aniketos.securebpmn.xacml.pdp.idHandler;
+
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import eu.aniketos.securebpmn.xacml.idm.AuthInfo;
+//import eu.aniketos.securebpmn.xacml.idm.IDInfo;
+
+public class IDInfoCache {
+
+//	private static Map<IDInfoKey, IDInfoCacheEntry> cache;
+//
+//	public IDInfoCache() {
+//		 cache = new HashMap<IDInfoKey, IDInfoCacheEntry>();
+//
+//		 //todo start thread which removes old entries
+//	}
+//
+//
+//
+//	/**
+//	 *
+//	 * @param idInfo
+//	 */
+//
+//	public synchronized void putEntry(IDInfo idInfo) {
+//		//cache.put(new IDInfoKey(), arg1)
+//	}
+//
+//	public void updateEntry(IDInfo idInfo) {
+//
+//	}
+//
+//	public IDInfoCacheEntry getEntry(IDInfo idInfo) {
+//		return null;
+//	}
+//
+//	class IDInfoKey {
+//		private String userID, idManagerID;
+//
+//		public IDInfoKey(String userID, String idManagerID) {
+//			this.userID = userID;
+//			this.idManagerID = idManagerID;
+//		}
+//
+//		@Override
+//		public int hashCode() {
+//			return this.userID.hashCode() + this.idManagerID.hashCode();
+//		}
+//	}
+//
+
+}
